25feef1a81474e4d751429bdf50ee51e8a2f6231
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-17 01:05:28 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,24 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts phrase = "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status
+  puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
 end
+
+def link
+  puts phrase = "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
+end
+  
+mario
+toadstool
+link
+all_phrases
+
+#one more quick question - is there any way to skip the autocompletion when i hit enter? for example, i'm typing a variable called variable_1 in and already have one called variable_10 so it autocompletes, but i didn't want that. How do I enter do go to the next line without autocompleting?
